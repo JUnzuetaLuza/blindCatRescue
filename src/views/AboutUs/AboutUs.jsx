704dfd0126235daabf0defd2d5a3f79a3327ed7d
JUnzuetaLuza <joseunz2k@gmail.com>
2025-01-15 18:54:10 -0500

    About and TheCats swap

--- src/views/AboutUs/AboutUs.jsx
+++ src/views/AboutUs/AboutUs.jsx
@@ -10,6 +10,7 @@ export const AboutUs = () => {
           <p>We would like to introduce toy to the blind, FIV & FELV+ residents of Blind Cat Rescue. Every cat may have more than one sponsor.<br/>We urge our supporters to choose wichever cat you find touches your heart the most, as they will all receive the same standard of care regardless of how many sponsors they have.</p>
           <p>It costs approximately $1000 per day to provide the cats with shelter, quality food, and medical care. Cats with NEED SPONSOR badge buttons currentlyhave two or less sponsors.<br/><br/>Thank you for your support in helping the cats!</p>
         </div>
+        
       </div>
     </>
   )
